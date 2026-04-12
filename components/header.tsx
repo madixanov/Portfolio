1f025ab9d8a55855f6632fd71d30ae0d81135e5f
ykDev <yoqub.madixanov@gmail.com>
2026-04-12 12:29:23 +0500

    Starting project

---
+++ components/header.tsx
@@ -0,0 +1,26 @@
+export default function Header() {
+    return (
+        <header>
+            // Logo
+            <div>
+                <h1>ykDev</h1>
+            </div>
+
+            // Nav links
+            <nav>
+                <ul>
+                    <li>About</li>
+                    <li>Skills</li>
+                    <li>Experience</li>
+                    <li>Projects</li>
+                    <li>Contacts</li>
+                </ul>
+            </nav>
+
+            // Contact button
+            <div>
+                <button>Contact Me</button>
+            </div>
+        </header>
+    )
+}
